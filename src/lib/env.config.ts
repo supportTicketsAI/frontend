76c1442cb5f00339c3f3ffaeaa553be15fe720fd
EstebanInnethic <96593734+EstebanR05@users.noreply.github.com>
2026-01-22 21:28:52 -0500

    fix: remove default value for API base URL in environment configuration

--- src/lib/env.config.ts
+++ src/lib/env.config.ts
@@ -1,5 +1,5 @@
 export const {
   VITE_SUPABASE_URL: supabaseUrl,
   VITE_SUPABASE_ANON_KEY: supabaseAnonKey,
-  VITE_API_BASE_URL: apiBaseUrl = 'http://localhost:8000/api/v1',
+  VITE_API_BASE_URL: apiBaseUrl,
 } = import.meta.env;
